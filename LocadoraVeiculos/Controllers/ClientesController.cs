5e5877f89bf34daa19df6dcf82eeb8092794132f
André Ravacini <andrelucasravacini@gmail.com>
2026-04-12 17:21:31 -0300

    Etapa 2.2 - CRUD de clientes

---
+++ LocadoraVeiculos/Controllers/ClientesController.cs
@@ -0,0 +1,71 @@
+﻿using LocadoraVeiculos.Data;
+using LocadoraVeiculos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocadoraVeiculos.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientesController : ControllerBase
+    {
+        private readonly LocadoraContext _context;
+
+        public ClientesController(LocadoraContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cliente>>> Get()
+        {
+            return await _context.Clientes.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cliente>> Get(int id)
+        {
+            var cliente = await _context.Clientes.FindAsync(id);
+
+            if (cliente == null)
+                return NotFound();
+
+            return cliente;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Cliente>> Post(Cliente cliente)
+        {
+            _context.Clientes.Add(cliente);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), new { id = cliente.IdCliente }, cliente);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Cliente cliente)
+        {
+            if (id != cliente.IdCliente)
+                return BadRequest();
+
+            _context.Entry(cliente).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cliente = await _context.Clientes.FindAsync(id);
+
+            if (cliente == null)
+                return NotFound();
+
+            _context.Clientes.Remove(cliente);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
