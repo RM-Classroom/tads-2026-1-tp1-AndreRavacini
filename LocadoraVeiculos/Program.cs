71b7e037f3eabfb75866d57e9ac8b376fed6d05c
André Ravacini <andrelucasravacini@gmail.com>
2026-04-12 17:02:15 -0300

    Etapa 2.1 - Configuracao inicial da API com Swagger

--- LocadoraVeiculos/Program.cs
+++ LocadoraVeiculos/Program.cs
@@ -9,30 +9,24 @@ namespace LocadoraVeiculos
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             builder.Services.AddDbContext<LocadoraContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.Run();
         }
     }
