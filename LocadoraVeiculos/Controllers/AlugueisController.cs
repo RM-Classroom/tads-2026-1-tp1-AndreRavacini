2234db341016574dea2d3f1e8580291023e7e8f1
André Ravacini <andrelucasravacini@gmail.com>
2026-04-12 17:28:14 -0300

    Etapa 2.2 - CRUD de aluguéis

---
+++ LocadoraVeiculos/Controllers/AlugueisController.cs
@@ -0,0 +1,77 @@
+﻿using LocadoraVeiculos.Data;
+using LocadoraVeiculos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocadoraVeiculos.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlugueisController : ControllerBase
+    {
+        private readonly LocadoraContext _context;
+
+        public AlugueisController(LocadoraContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Aluguel>>> Get()
+        {
+            return await _context.Alugueis
+                .Include(a => a.Cliente)
+                .Include(a => a.Veiculo)
+                .ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Aluguel>> Get(int id)
+        {
+            var aluguel = await _context.Alugueis
+                .Include(a => a.Cliente)
+                .Include(a => a.Veiculo)
+                .FirstOrDefaultAsync(a => a.IdAluguel == id);
+
+            if (aluguel == null)
+                return NotFound();
+
+            return aluguel;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Aluguel>> Post(Aluguel aluguel)
+        {
+            _context.Alugueis.Add(aluguel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), new { id = aluguel.IdAluguel }, aluguel);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Aluguel aluguel)
+        {
+            if (id != aluguel.IdAluguel)
+                return BadRequest();
+
+            _context.Entry(aluguel).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var aluguel = await _context.Alugueis.FindAsync(id);
+
+            if (aluguel == null)
+                return NotFound();
+
+            _context.Alugueis.Remove(aluguel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
